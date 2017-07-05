a1166fdb6226155ff65f12905400fd618c597698
BuildTools <unconfigured@null.spigotmc.org>
2017-07-05 16:09:27 +0100

    - Added createhorse and geldingtool commands. - Can now geld a stallion.

--- src/main/java/io/github/davidg95/equestricraftplugin/EquestriCraftPlugin.java
+++ src/main/java/io/github/davidg95/equestricraftplugin/EquestriCraftPlugin.java
@@ -10,23 +10,31 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Horse;
+import org.bukkit.entity.Player;
 import org.bukkit.entity.ThrownPotion;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.PotionSplashEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -53,6 +61,7 @@ public class EquestriCraftPlugin extends JavaPlugin implements Listener {
         properties = new Properties();
         loadProperties();
         container = DataContainer.getInstance();
+        getServer().getPluginManager().registerEvents(this, this);
         for (World w : worlds) {
             for (Horse h : w.getEntitiesByClass(Horse.class)) {
                 container.addHorse(h);
@@ -84,8 +93,73 @@ public class EquestriCraftPlugin extends JavaPlugin implements Listener {
             }
             sender.sendMessage("Horses: " + nohorses + "\nSick horses: " + sickhorses);
             return true;
+        } else if (cmd.getName().equalsIgnoreCase("createhorse")) {
+            if (sender instanceof Player) {
+                final Player player = (Player) sender;
+                if (args.length == 1) {
+                    final String genderarg = args[0];
+                    int gender;
+                    if (genderarg.equalsIgnoreCase("stallion")) {
+                        gender = MyHorse.STALLION;
+                    } else if (genderarg.equalsIgnoreCase("mare")) {
+                        gender = MyHorse.MARE;
+                    } else {
+                        sender.sendMessage("Unrecognised gender. Must be STALLION or MARE");
+                        return true;
+                    }
+                    final Horse h = player.getWorld().spawn(player.getLocation(), Horse.class);
+                    final MyHorse horse = new MyHorse(h);
+                    horse.setGender(gender);
+                }
+            } else {
+                sender.sendMessage("This command can only be run by a player");
+            }
+            return true;
+        } else if (cmd.getName().equalsIgnoreCase("geldingtool")) {
+            if (sender instanceof Player) {
+                final Player player = (Player) sender;
+                final PlayerInventory inventory = player.getInventory();
+                final ItemStack shears = new ItemStack(Material.SHEARS, 1);
+                final String shearsName = "Gelding Shears";
+                final ItemMeta im = shears.getItemMeta();
+                im.setDisplayName(shearsName);
+                final List<String> comments = new ArrayList<>();
+                comments.add("Used for gelding a Stallion");
+                im.setLore(comments);
+                shears.setItemMeta(im);
+                inventory.addItem(shears);
+            } else {
+                sender.sendMessage("Only players may use this command");
+            }
+            return true;
         }
         return false;
+    }
+
+    @EventHandler
+    public void onPlayerUse(EntityDamageByEntityEvent event) {
+        if (!(event.getDamager() instanceof Player)) { //Check the damager is a player.
+            return;
+        }
+        final Player player = (Player) event.getDamager(); //Get the player.
+        final ItemStack inHand = player.getItemInHand(); //Get the item in hand.
+        if (inHand == null || inHand.getType() != Material.SHEARS) { //Check they have shears in their hand.
+            return;
+        }
+        if (!inHand.getItemMeta().hasDisplayName()) { //Check the shears have a display name.
+            return;
+        }
+        if (!inHand.getItemMeta().getDisplayName().equals("Gelding Shears")) { //Check the shears are the Gelding Shears.
+            return;
+        }
+        if (event.getEntity() instanceof Horse) { //Check it was a horse they are hitting.
+            final MyHorse horse = container.getHorse((Horse) event.getEntity()); //Get the MyHorse instance.
+            if (horse.getGender() != MyHorse.STALLION) { //check it was a stallion.
+                player.sendMessage("This horse is not a stallion");
+                return;
+            }
+            horse.setGender(MyHorse.GELDING); //Turn the horse into a gelding.
+        }
     }
 
     @EventHandler
