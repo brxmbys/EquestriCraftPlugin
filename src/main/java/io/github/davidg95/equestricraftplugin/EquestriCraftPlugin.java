9a109919543ea60c75a23d5fdff3f4166c99ad09
BuildTools <unconfigured@null.spigotmc.org>
2017-07-07 18:05:01 +0100

    - Removed an import

--- src/main/java/io/github/davidg95/equestricraftplugin/EquestriCraftPlugin.java
+++ src/main/java/io/github/davidg95/equestricraftplugin/EquestriCraftPlugin.java
@@ -11,7 +11,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.Properties;
 import java.util.logging.Level;
