c44bb8e410594590da585298c54944275e91a46f
BuildTools <unconfigured@null.spigotmc.org>
2017-08-29 14:16:53 +0100

    JavaDoc

--- src/main/java/io/github/davidg95/equestricraftplugin/race/Race.java
+++ src/main/java/io/github/davidg95/equestricraftplugin/race/Race.java
@@ -13,20 +13,21 @@ import org.bukkit.entity.Player;
 import org.bukkit.metadata.FixedMetadataValue;
 
 /**
+ * Class which models a race.
  *
  * @author David
  */
 public class Race {
 
-    private final List<Player> players;
-    private final List<PlayerTime> complete;
+    private final List<Player> players; //The players entering the race.
+    private final List<PlayerTime> complete; //The name and the times of the players who have completed the race.
 
-    private CheckThread thread;
+    private CheckThread thread; //CheckerThread.
 
-    private boolean started;
-    private boolean finnished;
+    private boolean started; //Boolean indicating whether race has been started.
+    private boolean finnished; //Boolean indicating whether race has been complete.
 
-    private long startTime;
+    private long startTime; //Time the race started at.
 
     public Race() {
         players = new LinkedList<>();
@@ -35,6 +36,9 @@ public class Race {
         finnished = false;
     }
 
+    /**
+     * Starts the race.
+     */
     public void start() {
         started = true;
         startTime = new Date().getTime();
@@ -42,6 +46,9 @@ public class Race {
         thread.start();
     }
 
+    /**
+     * Finishes the race and displays the listings.
+     */
     public void finish() {
         finnished = true;
         Bukkit.broadcastMessage(ChatColor.BOLD + "" + ChatColor.GREEN + "RACE COMPLETE!");
@@ -59,11 +66,22 @@ public class Race {
         }
     }
 
+    /**
+     * Ends the race session immediately.
+     */
     public void terminate() {
         finnished = true;
+        thread.stopRun();
         Bukkit.broadcastMessage(ChatColor.BOLD + "" + ChatColor.RED + "RACE HAS BEEN TERMINATED!");
     }
 
+    /**
+     * Add a player to the race.
+     *
+     * @param p the player to add.
+     * @return true if the race has not yet started, false if the race has
+     * started already. If the race has stared, the player will not be added.
+     */
     public boolean addPlayer(Player p) {
         if (started) {
             return false;
@@ -72,10 +90,21 @@ public class Race {
         return true;
     }
 
+    /**
+     * Get all entrants.
+     *
+     * @return List of type Player.
+     */
     public List<Player> getPlayers() {
         return players;
     }
 
+    /**
+     * Calculates the players time and adds them to the complete list. Displays
+     * their time and position.
+     *
+     * @param p the player.
+     */
     public void completePlayer(Player p) {
         final long time = new Date().getTime();
         final long raceTime = time - startTime;
@@ -92,22 +121,46 @@ public class Race {
         p.setMetadata("time", new FixedMetadataValue(EquestriCraftPlugin.plugin, time));
     }
 
-    public List getCompletedPlayers() {
+    /**
+     * Get the complete players and times.
+     *
+     * @return List of type PlayerTime.
+     */
+    public List<PlayerTime> getCompletedPlayers() {
         return complete;
     }
 
+    /**
+     * Remove a player from the race.
+     *
+     * @param player the player to remove.
+     * @return true if they were in the race, false if they were not.
+     */
     public boolean withdraw(Player player) {
         return players.remove(player);
     }
 
+    /**
+     * Check if the race has finished.
+     *
+     * @return boolean indicating state.
+     */
     public boolean isFinnsihed() {
         return finnished;
     }
 
+    /**
+     * Check if the race has started.
+     *
+     * @return boolean indicating state.
+     */
     public boolean isStarted() {
         return started;
     }
 
+    /**
+     * Class modeling player time.
+     */
     public class PlayerTime {
 
         private String name;
