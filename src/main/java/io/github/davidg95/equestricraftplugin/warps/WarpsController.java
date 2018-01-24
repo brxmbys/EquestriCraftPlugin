58c66f267f2360aca7ecfe45e5a60a6ba71bebe6
davidg95 <pirakaleader@googlemail.com>
2018-01-24 12:22:58 +0000

    Bug fix when viewing build logs. Can now tp to a players private warp

--- src/main/java/io/github/davidg95/equestricraftplugin/warps/WarpsController.java
+++ src/main/java/io/github/davidg95/equestricraftplugin/warps/WarpsController.java
@@ -6,12 +6,16 @@ package io.github.davidg95.equestricraftplugin.warps;
 import io.github.davidg95.equestricraftplugin.EquestriCraftPlugin;
 import io.github.davidg95.equestricraftplugin.database.Database;
 import java.util.List;
+import java.util.UUID;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.permissions.Permission;
 
 /**
  *
@@ -21,6 +25,7 @@ public class WarpsController implements CommandExecutor {
 
     private final EquestriCraftPlugin plugin;
     private final Database database;
+    private final Permission gotoPerm = new Permission("equestricraft.pwarp.goto");
 
     public WarpsController(EquestriCraftPlugin plugin, Database database) {
         this.plugin = plugin;
@@ -75,6 +80,24 @@ public class WarpsController implements CommandExecutor {
                 player.teleport(warp.getLocation());
                 return true;
             }
+        } else if (args[0].equalsIgnoreCase("go-to")) {
+            if (!(sender instanceof Player)) {
+                sender.sendMessage("Only players can use this command!");
+                return true;
+            }
+            Player player = (Player) sender;
+            if (player.hasPermission(gotoPerm)) {
+                if (args.length >= 3) {
+                    OfflinePlayer p = Bukkit.getOfflinePlayer(UUID.fromString(args[1]));
+                    String name = args[2];
+                    Warp warp = database.getPlayerWarp(p, name);
+                    player.sendMessage("Teleporting to " + p.getName() + ":" + name + "...");
+                    player.teleport(warp.getLocation());
+                }
+            } else {
+                player.sendMessage(ChatColor.RED + "You do not have permission to go to other players pwarps");
+            }
+            return true;
         } else if (args[0].equalsIgnoreCase("show-all")) {
             if (!(sender instanceof Player)) {
                 List<Warp> warps = database.getAllWarps();
